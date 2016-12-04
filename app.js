440f41eec40ccab6a5af4a7ddd69a813497dc46a
Martin Salles <salles.martin@gmail.com>
2016-12-04 21:32:59 +0100

    Words are editable

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var express = require('express');
+var morgan = require('morgan');
+
 var app = express();
+app.use(morgan('combined'));
+
+var methodOverride = require('method-override')
+app.use(methodOverride('_method'));
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -7,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
+
 
 var Word = function(french, italian) {
   this.french = french;
@@ -18,12 +25,13 @@ var words = [
   new Word("souris", "toppo")
 ];
 
+
 app.get('/', function(req, res) {
   res.redirect('/words');
 })
 
 app.get('/words', function (req, res) {
-  res.render('words', {
+  res.render('index', {
     words: words,
     title: "Known words"
   });
@@ -40,6 +48,24 @@ app.post('/words', function(req,res) {
   var word = new Word(body.french, body.italian);
   words.push(word);
 
+  res.redirect('/');
+});
+
+app.get('/words/:id/edit', function(req, res) {
+  var word = words[req.params.id];
+  res.render('edit', {
+    title: "Edit a word",
+    word: word,
+    id: req.params.id
+  });
+});
+
+app.put('/words/:id/update', function(req,res) {
+  var word = words[req.params.id];
+  var body = req.body;
+  word.french = body.french;
+  word.italian = body.italian;
+  
   res.redirect('/');
 });
 
