1d040c30d1acb97f1b309d4953a4d724e6d77fde
Martin Salles <salles.martin@gmail.com>
2016-12-13 17:43:39 +0100

    Store words in mongo db

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # italian-learning
 Just trying stuff on expressjs
+
+
+### Installation
+
+
+From the project folder:
+`npm install`
+
+Install mongodb:
+https://docs.mongodb.com/manual/administration/install-community/
+
+`node app.js`
--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var morgan = require('morgan');
 
+const mongo = require('mongodb'), assert = require('assert');
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -14,26 +16,21 @@ app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 
-
 var Word = function(french, italian) {
   this.french = french;
   this.italian = italian;
 }
-var words = [
-  new Word("chien", "cane"),
-  new Word("chat", "gatto"),
-  new Word("souris", "toppo")
-];
-
 
 app.get('/', function(req, res) {
   res.redirect('/words');
 })
 
 app.get('/words', function (req, res) {
-  res.render('index', {
-    words: words,
-    title: "Known words"
+  db.collection('words').find().toArray((err, words) => {
+    res.render('index', {
+      words: words,
+      title: "Known words"
+    });
   });
 });
 
@@ -46,29 +43,43 @@ app.get('/words/new', function(req, res) {
 app.post('/words', function(req,res) {
   var body = req.body;
   var word = new Word(body.french, body.italian);
-  words.push(word);
+
+  db.collection('words').insertOne(word, function(err, r) {
+    assert.equal(null, err);
+    assert.equal(1, r.insertedCount);
+  });
 
   res.redirect('/');
 });
 
 app.get('/words/:id/edit', function(req, res) {
-  var word = words[req.params.id];
-  res.render('edit', {
-    title: "Edit a word",
-    word: word,
-    id: req.params.id
+  db.collection('words').findOne({'_id': new mongo.ObjectId(req.params.id)}, function(err, word) {
+    assert.equal(err, null);
+
+    res.render('edit', {
+      title: "Edit a word",
+      word: word
+    });
   });
 });
 
-app.put('/words/:id/update', function(req,res) {
-  var word = words[req.params.id];
+app.put('/words/:id/update', function(req, res) {
   var body = req.body;
-  word.french = body.french;
-  word.italian = body.italian;
-  
-  res.redirect('/');
+  var word = new Word(body.french, body.italian)
+
+  db.collection('words').findOneAndUpdate({'_id': new mongo.ObjectId(req.params.id)}, word, function(err, word) {
+    assert.equal(err, null);
+
+    res.redirect('/');
+  });
 });
 
-app.listen(3000, function(){
-  console.log("Listening on port 3000...");
-});
+var db;
+mongo.MongoClient.connect('mongodb://localhost:27017/italian-learning', (err, database) => {
+  if (err) return console.log(err);
+  db = database;
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000...');
+  })
+})
